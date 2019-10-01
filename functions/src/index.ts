e1dce13e2d4042e36504eb135a25b24709529864
sebastzr <sebastzr@utp.edu.co>
2019-10-01 10:45:04 -0500

    Feat: Cloud functions CSV Report

---
+++ functions/src/index.ts
@@ -0,0 +1,53 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+const json2csv = require("json2csv").parse;
+
+admin.initializeApp();
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.jsonReport = functions.https.onRequest( (request, response) => {
+    const db = admin.firestore();
+    let data:any = [];
+    db.collection('_forms').get()
+        .then( (querySnapshot) => {
+            querySnapshot.forEach( (doc) => {
+                data.push(doc.data());
+            });
+            response.status(200).json(data);         
+        }).catch( (error) => {
+            console.log('Error gettin document: ', error);
+            response.status(200).send('Error gettin document: ' + error);
+        });
+});
+
+exports.cssJsonReport = functions.https.onRequest( (request, response) => {
+    const db = admin.firestore();
+    let data: any = [];
+    db.collection('_forms').get()
+        .then( (querySnapshot) => {
+            querySnapshot.forEach( (doc) => {
+                data.push(doc.data());
+            });
+            const csv = json2csv(data, {
+                flatten: true,                
+                delimiter: ';',
+                excelStrings: true,
+                includeEmptyRows: true,
+            });  
+            response.setHeader(
+                'Content-disposition',
+                'attachment; filename=report.csv'
+            )
+            response.set('Content-type', 'text/csv');
+            response.status(200).send(csv);                   
+        }).catch( (error) => {
+            console.log('Error gettin document: ', error);
+            response.status(200).send('Error gettin document: ' + error);
+        });    
+});
