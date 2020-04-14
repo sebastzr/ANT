f451f9949baa20be056af733d6e83e04843ea107
Sebastian <sebastzr@utp.edu.co>
2020-04-14 16:47:42 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAxaWmFO1W0pPIuaTK6mnZTLZKEttFVN2Y",
-    authDomain: "ant-app-7ee04.firebaseapp.com",
-    databaseURL: "https://ant-app-7ee04.firebaseio.com",
-    projectId: "ant-app-7ee04",
-    storageBucket: "",
-    messagingSenderId: "593354332989",
-    appId: "1:593354332989:web:7d7408657cce6b84"
+    //Insert firebase project config
   }
 };
